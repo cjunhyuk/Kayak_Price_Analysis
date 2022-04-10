5933b11cb543fc328bc676e8021b46562e206fed
Jun Choi <cjunhyuk@yahoo.com>
2022-04-10 18:22:07 -0400

    Moved all py files into its own folder for clean up

--- py_files/mac_scraper.py
+++ py_files/mac_scraper.py
@@ -8,13 +8,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 import smtplib
 from email.mime.multipart import MIMEMultipart
 import os
 
-current_path = os.getcwd()
-driver = webdriver.Chrome('chromedriver.exe')
+driver = webdriver.Chrome(ChromeDriverManager().install())
 sleep(2)
 
 # Load more results to maximize the scraping
@@ -84,10 +84,98 @@ def start_kayak(city_from, city_to, date_start, date_end):
     # saving a new dataframe as an excel file. the name is custom made to your cities and dates
     final_df = df_flights_cheap.append(df_flights_best).append(df_flights_fast)
 
-    final_df.to_csv('scraped data/{}_flights_{}-{}_from_{}_to_{}.csv'.format(strftime("%Y%m%d-%H%M"),
+    final_df.to_csv('scraped_data/{}_flights_{}-{}_from_{}_to_{}.csv'.format(strftime("%Y%m%d-%H%M"),
                                                                                    city_from, city_to, 
                                                                                    date_start, date_end), index=False)
     print('saved df.....')
+
+# Scraper
+    
+# def page_scrape():
+#     """This function takes care of the scraping part"""
+    
+#     xp_sections = '//*[@class="section duration allow-multi-modal-icons"]'
+#     sections = driver.find_elements(By.XPATH, xp_sections)
+#     sections_list = [value.text for value in sections if len(value.text) != 0]
+#     section_a_list = sections_list[::2] # This is to separate the two flights
+#     section_b_list = sections_list[1::2] # This is to separate the two flights
+    
+#     # if you run into a reCaptcha, you might want to do something about it
+#     # you will know there's a problem if the lists above are empty
+#     # this if statement lets you exit the bot or do something else
+#     # you can add a sleep here, to let you solve the captcha and continue scraping
+#     # i'm using a SystemExit because i want to test everything from the start
+#     if section_a_list == []:
+#         raise SystemExit
+    
+#     # I'll use the letter A for the outbound flight and B for the inbound
+#     a_duration = []
+#     a_section_names = []
+#     for n in section_a_list:
+#         # Separate the time from the cities
+#         a_section_names.append(''.join(n.split()[2:5]))
+#         a_duration.append(''.join(n.split()[0:2]))
+#     b_duration = []
+#     b_section_names = []
+#     for n in section_b_list:
+#         # Separate the time from the cities
+#         b_section_names.append(''.join(n.split()[2:5]))
+#         b_duration.append(''.join(n.split()[0:2]))
+
+#     # getting the prices
+#     xp_prices = '//span[@class="price option-text"]'
+#     prices = driver.find_elements(By.XPATH, xp_prices)
+#     prices_list = [price.text.replace('$','').replace(',','') for price in prices if price.text != '']
+#     prices_list = list(map(int, prices_list))
+
+#     # the stops are a big list with one leg on the even index and second leg on odd index
+#     xp_stops = '//div[@class="section stops"]/div[1]'
+#     stops = driver.find_elements(By.XPATH, xp_stops)
+#     stops_list = [stop.text[0].replace('n','0') for stop in stops if len(stop.text) != 0]
+#     a_stop_list = stops_list[::2]
+#     b_stop_list = stops_list[1::2]
+
+#     xp_stops_cities = '//div[@class="section stops"]/div[2]'
+#     stops_cities = driver.find_elements(By.XPATH, xp_stops_cities)
+#     stops_cities_list = [stop.text for stop in stops_cities if len(stop.text) != 0]
+#     a_stop_name_list = stops_cities_list[::2]
+#     b_stop_name_list = stops_cities_list[1::2]
+    
+#     # this part gets me the airline company and the departure and arrival times, for both legs
+#     xp_schedule = '//div[@class="section times"]'
+#     schedules = driver.find_elements(By.XPATH, xp_schedule)
+#     hours_list = []
+#     carrier_list = []
+#     for schedule in schedules:
+#         if len(schedule.text) != 0:
+#             hours_list.append(schedule.text.split('\n')[0])
+#             carrier_list.append(schedule.text.split('\n')[1])
+#     # split the hours and carriers, between a and b legs
+#     a_hours = hours_list[::2]
+#     a_carrier = carrier_list[::2]
+#     b_hours = hours_list[1::2]
+#     b_carrier = carrier_list[1::2]
+    
+#     cols = (['Out Time', 'Out Airline', 'Out Cities', 'Out Duration', 'Out Stops', 'Out Stop Cities',
+#             'Return Time', 'Return Airline', 'Return Cities', 'Return Duration', 'Return Stops', 'Return Stop Cities',
+#             'Price'])    
+
+#     flights_df = pd.DataFrame({'Out Duration': a_duration,
+#                            'Out Cities': a_section_names,
+#                            'Return Duration': b_duration,
+#                            'Return Cities': b_section_names,
+#                            'Out Stops': a_stop_list,
+#                            'Out Stop Cities': a_stop_name_list,
+#                            'Return Stops': b_stop_list,
+#                            'Return Stop Cities': b_stop_name_list,
+#                            'Out Time': a_hours,
+#                            'Out Airline': a_carrier,
+#                            'Return Time': b_hours,
+#                            'Return Airline': b_carrier,                           
+#                            'Price': prices_list})[cols]
+    
+#     flights_df['timestamp'] = strftime("%Y%m%d-%H%M") # so we can know when it was scraped
+#     return flights_df
 
 # Scraper
     
